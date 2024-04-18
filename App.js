1546c7ea0783817fa303e65d7ced7998547738ce
Kamlesh <bankeykamlesh142003@gmail.com>
2024-04-18 21:59:56 +0530

    Change App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import {View, Text} from 'react-native';
 const App = () => {
   return <View style={{flex:1}}>
     <Text>
-      Hello World ! How 
+      Hello World ! How Are You 
     </Text>
   </View>;
 };
